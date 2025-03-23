48c05b387a470ebd2e946734b1013a49d97fbe5d
maximilian-hub <maximilian.spedale@gmail.com>
2025-03-22 21:01:29 -0500

    finished explosion

--- nonsense/explosion.gd
+++ nonsense/explosion.gd
@@ -2,7 +2,8 @@ extends AnimatedSprite2D
 
 func _ready():
 	play("explode") # Optional if you didn’t check “Autoplay on Load”
+	$AudioStreamPlayer2D.pitch_scale = randf_range(0.95, 1.05)
+	$AudioStreamPlayer2D.play()
 
 func _on_animation_finished():
-	print("animation finished.")
 	queue_free()
