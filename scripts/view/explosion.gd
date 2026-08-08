8a0d4e3bd6f903112bf585405457c80fbf4a7ba5
maximilian-hub <maximilian.spedale@gmail.com>
2026-08-08 11:34:31 -0500

    Fixed explosion error.

--- scripts/view/explosion.gd
+++ scripts/view/explosion.gd
@@ -1,7 +1,7 @@
 extends AnimatedSprite2D
 
 func _ready():
-	play("explode") # Optional if you didn’t check “Autoplay on Load”
+	#play("explosion") # Optional if you didn’t check “Autoplay on Load”
 	$AudioStreamPlayer2D.pitch_scale = randf_range(0.75, 1.25)
 	$AudioStreamPlayer2D.play()
 
